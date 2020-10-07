c32a8645b3f2a95674136dbf4bb755ec864a905f
Michelle Charron <michelle.charron93@gmail.com>
2020-10-07 13:39:07 -0600

    Added button function wtih comparison for choice

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ function play(playerChoice){
   let compChoice = 'rock'
 
   if(playerChoice == 'rock'){
-    console.log('rock')
-
+    compareChoice('rock')
+    // console.log('rock', 'The Computer chose', compChoice)
   }else if(playerChoice == 'paper'){
-    console.log('paper')
+    compareChoice('paper')
   }else if(playerChoice == 'scissors'){
-    console.log('scissors')
+    compareChoice('scissors')
   }
+}
+
+function compareChoice(item){
+  let compChoice = 'rock'
+
+    if(item == 'rock'){
+      console.log('its a tie')
+    }else if(item == 'paper'){
+      console.log('paper beats rock')
+    }else if(item == 'scissors'){
+      console.log('rock beats scissors')
+    }
+    else{
+      console.log("this didn't work")
+    }
+
 }
