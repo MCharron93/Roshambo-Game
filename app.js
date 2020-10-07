b3b47bba6b286576f0afd44b47f1b8a26f793995
Michelle Charron <michelle.charron93@gmail.com>
2020-10-07 13:22:06 -0600

    added buttons and beginning app.js functions

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+function play(playerChoice){
+  let compChoice = 'rock'
+
+  if(playerChoice == 'rock'){
+    console.log('rock')
+
+  }else if(playerChoice == 'paper'){
+    console.log('paper')
+  }else if(playerChoice == 'scissors'){
+    console.log('scissors')
+  }
+}
